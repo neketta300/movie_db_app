6299abc8bdcf114ff312b8b3161525d2232b2ace
neketta300 <neketta300@gmail.com>
2025-03-22 16:46:35 +0300

    add my_app

---
+++ lib/ui/widgets/app/my_app.dart
@@ -0,0 +1,40 @@
+import 'package:moviedb_app_llf/ui/theme/app_colors.dart';
+import 'package:moviedb_app_llf/ui/widgets/auth/auth_model.dart';
+import 'package:moviedb_app_llf/ui/widgets/auth/auth_widget.dart';
+import 'package:moviedb_app_llf/ui/widgets/main_screen/main_screen_widget.dart';
+import 'package:moviedb_app_llf/ui/widgets/movie_details/movie_details_widget.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(backgroundColor: AppColors.mainDarkBlue),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: AppColors.mainDarkBlue,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+        ),
+      ),
+      routes: {
+        '/auth':
+            (context) =>
+                AuthProvider(model: AuthModel(), child: const AuthWidget()),
+        '/main_screen': (context) => const MainScreenWidget(),
+        '/main_screen/movie_details': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments;
+          if (arguments is int) {
+            return MovieDetailsWidget(movieId: arguments);
+          } else {
+            return MovieDetailsWidget(movieId: 0);
+          }
+        },
+      },
+      initialRoute: '/auth',
+    );
+  }
+}
