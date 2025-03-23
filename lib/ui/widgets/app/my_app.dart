2d5bcfe7e18007c7fa082838c2f1c03ef0d77dad
neketta300 <neketta300@gmail.com>
2025-03-23 11:18:53 +0300

    refactoring routes

--- lib/ui/widgets/app/my_app.dart
+++ lib/ui/widgets/app/my_app.dart
@@ -1,12 +1,12 @@
+import 'package:moviedb_app_llf/ui/navigation/main_navigation.dart';
 import 'package:moviedb_app_llf/ui/theme/app_colors.dart';
-import 'package:moviedb_app_llf/ui/widgets/auth/auth_model.dart';
-import 'package:moviedb_app_llf/ui/widgets/auth/auth_widget.dart';
-import 'package:moviedb_app_llf/ui/widgets/main_screen/main_screen_widget.dart';
-import 'package:moviedb_app_llf/ui/widgets/movie_details/movie_details_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:moviedb_app_llf/ui/widgets/app/my_app_model.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final MyAppModel model;
+  static final mainNavigation = MainNavigation();
+  const MyApp({super.key, required this.model});
 
   @override
   Widget build(BuildContext context) {
@@ -20,21 +20,9 @@ class MyApp extends StatelessWidget {
           unselectedItemColor: Colors.grey,
         ),
       ),
-      routes: {
-        '/auth':
-            (context) =>
-                AuthProvider(model: AuthModel(), child: const AuthWidget()),
-        '/main_screen': (context) => const MainScreenWidget(),
-        '/main_screen/movie_details': (context) {
-          final arguments = ModalRoute.of(context)?.settings.arguments;
-          if (arguments is int) {
-            return MovieDetailsWidget(movieId: arguments);
-          } else {
-            return MovieDetailsWidget(movieId: 0);
-          }
-        },
-      },
-      initialRoute: '/auth',
+      routes: mainNavigation.routes,
+      initialRoute: mainNavigation.initialRoute(model.isAuth),
+      onGenerateRoute: mainNavigation.onGenerateRoute,
     );
   }
 }
