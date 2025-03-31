2b105087c7489b01c467cbc9bd6faca28b301790
neketta300 <neketta300@gmail.com>
2025-03-31 15:27:58 +0300

    add new movie details model

---
+++ lib/ui/widgets/movie_details/movie_details_model.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'package:moviedb_app_llf/domain/api_client/api_client.dart';
+import 'package:moviedb_app_llf/domain/entity/movie_details.dart';
+
+class MovieDetailsModel extends ChangeNotifier {
+  final _apiClient = ApiClient();
+
+  final int movieId;
+  late DateFormat _dateFomat;
+  String _locale = '';
+  MovieDetails? _movieDetails;
+
+  MovieDetails? get movieDetails => _movieDetails;
+
+  MovieDetailsModel({required this.movieId});
+
+  Future<void> setUpLocale(BuildContext context) async {
+    final locale = Localizations.localeOf(context).toLanguageTag();
+    //print(_locale);
+    if (_locale == locale) return;
+    _locale = locale;
+    _dateFomat = DateFormat.yMMMd(locale);
+    await loadDetails();
+  }
+
+  Future<void> loadDetails() async {
+    _movieDetails = await _apiClient.movieDetails(movieId, _locale);
+    notifyListeners();
+  }
+}
