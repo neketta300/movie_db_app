30ceaf34ebf0005b5a378476e6d2f276fd625551
neketta300 <neketta300@gmail.com>
2025-04-12 16:32:33 +0300

    add reset session method

--- lib/ui/widgets/app/my_app_model.dart
+++ lib/ui/widgets/app/my_app_model.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:moviedb_app_llf/domain/data_providers/session_data_provider.dart';
+import 'package:moviedb_app_llf/ui/navigation/main_navigation.dart';
 
 class MyAppModel {
   final _sessionDataProvider = SessionDataProvider();
@@ -8,5 +10,13 @@ class MyAppModel {
   Future<void> checkAuth() async {
     final sessionId = await _sessionDataProvider.getSessionId();
     _isAuth = sessionId != null;
+  }
+
+  Future<void> resetSession(BuildContext context) async {
+    await _sessionDataProvider.setAccountId(null);
+    await _sessionDataProvider.setSessionId(null);
+    Navigator.of(
+      context,
+    ).pushNamedAndRemoveUntil(MainNavigationRoutesName.auth, (route) => false);
   }
 }
